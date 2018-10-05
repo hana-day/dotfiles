64ce7327e619c4cbdeae083292d3a1629aa3c6c0
Yusuke Hanada <hyusuk9872@gmail.com>
2018-10-06 01:01:39 +0900

    Fix web mode settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -242,14 +242,17 @@
 
 (use-package web-mode
   :ensure t
+  :init
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-expanding t)
+  (setq web-mode-enable-css-colorization t)
   :config
   (add-to-list 'auto-mode-alist
-               '("\\.\\(html\\|erb\\|css\\|vue\\|jsx\\|js\\|php\\)$" . web-mode))
-  (add-hook 'web-mode-hook '(lambda ()
-                              (setq web-mode-markup-indent-offset 2)
-                              (setq web-mode-auto-close-style t)
-                              (setq web-mode-tag-auto-close-style t)
-                              (setq web-mode-enable-auto-pairing t))))
+               '("\\.\\(html\\|erb\\|css\\|vue\\|jsx\\|js\\|php\\)$" . web-mode)))
 
 (use-package yasnippet
   :ensure t
